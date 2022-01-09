7384052840e6a2b23757d2a27f0548f408afe025
AetheryNA <mrnaaw98@gmail.com>
2022-01-09 21:41:07 +0530

    Setup files :smile:

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      primary: '#fff',
+      secondary: '#000'
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
