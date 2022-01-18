5d56da059db4a8efa40bd78a180e7e5ea87ee7a7
Naveed Ahamed <mrnaaw98@gmail.com>
2022-01-18 14:37:24 +0530

    [F002] Footer (#3)
    * Added Heroicons package, added new SVGs, reconfigured Tailwind and Next Config to support SVG imports
    
    * Added new Font type, added new color
    
    * Footer component built
    
    * Added a hover effect to the footer
    
    * Made changes to pass checks :)

---
+++ src/components/Footer.js
@@ -0,0 +1,26 @@
+import LinkedIn from '../../public/icons/linkedin.svg'
+import Github from '../../public/icons/github.svg'
+import {MailIcon} from '@heroicons/react/solid'
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="footer__wrapper flex flex-row max-w-max ml-auto">
+          <h3 className="mr-16"> Reach out to me </h3>
+          <a href="mailto:mrnaaw98@gmail.com" target="_blank" rel="noreferrer">
+            <MailIcon />
+          </a>
+          <a href="https://www.linkedin.com/in/naveed-ahamed/" target="_blank" rel="noreferrer">
+            <LinkedIn />
+          </a>
+          <a href="https://github.com/AetheryNA" target="_blank" rel="noreferrer">
+            <Github /> 
+          </a>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,10 @@
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false,
   theme: {
     colors: {
       primary: '#fff',
       secondary: '#000'
-    },
-    fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
     },
     extend: {},
   },
