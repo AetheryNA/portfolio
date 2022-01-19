7c2200bf34814a2ddc95af85497920ec2999c69a
AetheryNA <mrnaaw98@gmail.com>
2022-01-19 21:46:11 +0530

    New tailwind configuration and added placeholder images to trusted links

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   reactStrictMode: true,
 
+  images: {
+    domains: ['picsum.photos'],
+  },
+
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,12 @@ module.exports = {
   darkMode: false,
   theme: {
     colors: {
-      primary: '#fff',
-      secondary: '#000'
+      primary: '#90DDFF',
+      black: {
+        100: '#4E4E4E',
+        500: '#000',
+      },
+      white: '#fff',
     },
     extend: {},
   },
