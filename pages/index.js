d4c349fe0a50e32566419aa0a8428f5d1bb0fbca
AetheryNA <mrnaaw98@gmail.com>
2022-01-19 20:29:38 +0530

    Intro header, H2 styling

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import Footer from "../src/components/Footer"
+import IntroTitle from "../src/components/IntroTitle"
 
 export default function Home() {
   return (
     <>
+      <div className="container">
+        <IntroTitle title={'Hi! I build things on the Web.'} />
+      </div>
       <Footer />
     </>
   )
