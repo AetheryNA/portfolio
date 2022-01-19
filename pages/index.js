852edee8ce3b0e2eb625974b8bae4f7fbfbe7739
AetheryNA <mrnaaw98@gmail.com>
2022-01-19 21:46:35 +0530

    About card styling :)

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Footer from "../src/components/Footer"
 import IntroTitle from "../src/components/IntroTitle"
+import AboutCard from "../src/components/AboutCard"
 
 export default function Home() {
   return (
     <>
       <div className="container">
         <IntroTitle title={'Hi! I build things on the Web.'} />
+        <AboutCard />
       </div>
       <Footer />
     </>
