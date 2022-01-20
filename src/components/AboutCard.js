0f911b1b2d3ae8c9da9557a53a7cc0fc2058856b
AetheryNA <mrnaaw98@gmail.com>
2022-01-20 20:03:34 +0530

    Fixed failed checks :)

--- src/components/AboutCard.js
+++ src/components/AboutCard.js
@@ -1,15 +1,20 @@
 import {UserIcon} from '@heroicons/react/solid'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const AboutCard = () => {
   return (
-    <a href='/' className="about-card block relative mt-4 rounded-lg max-w-max">
-      <Image src="/images/aboutme.jpg" width={400} height={485} className="rounded-lg object-cover" />
-      <div className="about-card__details flex flex-row items-center absolute bottom-5 left-5 lg:bottom-0 lg:left-0 p:3 lg:p-5">
-        <UserIcon className="w-5 h-5 lg:w-10 lg:h-10 text-white" />
-        <p className='ml-2.5 font-semibold text-white'>about me</p>
-      </div>
-    </a>
+    <div className="about-card block relative mt-4 rounded-lg max-w-max">
+      <Link href='/' >
+        <>
+          <Image src="/images/aboutme.jpg" width={400} height={485} className="rounded-lg object-cover" />
+            <div className="about-card__details flex flex-row items-center absolute bottom-5 left-5 lg:bottom-0 lg:left-0 p:3 lg:p-5">
+              <UserIcon className="w-5 h-5 lg:w-10 lg:h-10 text-white" />
+              <p className='ml-2.5 font-semibold text-white'>about me</p>
+            </div>
+        </>
+      </Link>
+    </div>
   );
 };
 
