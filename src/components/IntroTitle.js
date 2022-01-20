667ac7468c8b909f1f4a414000296b35e0a5239d
Naveed Ahamed <mrnaaw98@gmail.com>
2022-01-20 20:15:33 +0530

    [F003] About me (#7)
    * Intro header, H2 styling
    
    * Missing styling
    
    * New tailwind configuration and added placeholder images to trusted links
    
    * About card styling :)
    
    * Changed placeholder image
    
    * Added tailwind configs for screens, adjusted about me card
    
    * Fixed failed checks :)
    
    * Adjusted styling

---
+++ src/components/IntroTitle.js
@@ -0,0 +1,9 @@
+const IntroTitle = ({ title }) => {
+  return (
+    <div className="intro-title mt-20">
+      <h2 className="relative max-w-max">{title}</h2>
+    </div>
+  );
+};
+
+export default IntroTitle;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,19 @@ module.exports = {
   darkMode: false,
   theme: {
     colors: {
-      primary: '#fff',
-      secondary: '#000'
+      primary: '#90DDFF',
+      black: {
+        100: '#4E4E4E',
+        500: '#000',
+      },
+      white: '#fff',
+    },
+    screens: {
+      'xs' : '640px',
+      's' : '768px',
+      'md' : '1024px',
+      'lg' : '1280px',
+      'xl' : '1600px',
     },
     extend: {},
   },
