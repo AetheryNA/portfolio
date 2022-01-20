b649464b6be42b400fa420de9e4968d91496988f
AetheryNA <mrnaaw98@gmail.com>
2022-01-20 19:49:25 +0530

    Changed placeholder image

--- src/components/AboutCard.js
+++ src/components/AboutCard.js
@@ -3,13 +3,13 @@ import Image from 'next/image'
 
 const AboutCard = () => {
   return (
-    <div className="about-card relative mt-4 rounded-lg max-w-max">
-      <Image src="https://picsum.photos/1000" width={400} height={485} className="rounded-lg" />
+    <a href='/' className="about-card block relative mt-4 rounded-lg max-w-max">
+      <Image src="/images/aboutme.jpg" width={400} height={485} className="rounded-lg object-cover" />
       <div className="about-card__details flex flex-row items-center absolute bottom-0 left-0 p:3 lg:p-5">
         <UserIcon className="w-5 h-5 lg:w-10 lg:h-10 text-white" />
         <p className='ml-2.5 font-semibold text-white'>about me</p>
       </div>
-    </div>
+    </a>
   );
 };
 
