6275a7da4f25dbd5b2080894daf8f4141d1c86a0
Naveed Ahamed <mrnaaw98@gmail.com>
2022-01-22 16:12:27 +0530

    [F005] Timeline Slider (#8)
    * Added Swiper to project
    
    * Changes to about me card
    
    * Added new styling, changes to home page and created the timeline slider component

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,17 @@
 import Footer from "../src/components/Footer"
 import IntroTitle from "../src/components/IntroTitle"
 import AboutCard from "../src/components/AboutCard"
+import Timeline from "../src/components/Timeline"
 
 export default function Home() {
   return (
     <>
-      <div className="container">
+      <div className="container overflow-visible">
         <IntroTitle title={'Hi! I build things on the Web.'} />
-        <AboutCard />
+        <div className="portfolio-main__wrapper pt-4 grid">
+          <AboutCard />
+          <Timeline />
+        </div>
       </div>
       <Footer />
     </>
