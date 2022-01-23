d1bf88470861fc21090403e967417d121e13c2b5
Naveed Ahamed <mrnaaw98@gmail.com>
2022-01-23 14:52:24 +0530

    [F007] About me (#10)
    * Intro title made to match the design
    
    * Swiper slideshow and about me card styling
    
    * Made box shadow lighter

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,20 @@ import Footer from "../src/components/Footer"
 import IntroTitle from "../src/components/IntroTitle"
 import AboutCard from "../src/components/AboutCard"
 import Timeline from "../src/components/Timeline"
+import Slideshow from "../src/components/Slideshow"
 
 export default function Home() {
   return (
     <>
       <div className="container overflow-visible">
-        <IntroTitle title={'Hi! I build things on the Web.'} />
-        <div className="portfolio-main__wrapper pt-4 grid">
+        <IntroTitle title={'I build things on the Web.'} />
+        <div className="portfolio-main__wrapper pt-4 md:grid hidden">
           <AboutCard />
           <Timeline />
         </div>
+      </div>
+      <div className="portfolio-main__wrapper pt-4 md:hidden">
+        <Slideshow />
       </div>
       <Footer />
     </>
