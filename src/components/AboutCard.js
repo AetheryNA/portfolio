29a7274900e5ac6e133f9f22b47f537011d5b877
AetheryNA <mrnaaw98@gmail.com>
2022-01-30 18:28:37 +0530

    Fixed CICD checks

--- src/components/AboutCard.js
+++ src/components/AboutCard.js
@@ -14,7 +14,7 @@ const AboutCard = () => {
     <div className="about-card block relative rounded-lg max-w-max w-full cursor-pointer" onClick={navigationToSection}>
       <Link href='#about-me'>
         <>
-          <Image src="/images/aboutme.jpg" width={400} height={485} className="rounded-lg object-cover" />
+          <Image src="/images/aboutme.jpg" width={400} height={485} className="rounded-lg object-cover" alt='about-me-card' />
             <div className="about-card__details flex flex-row items-center absolute bottom-5 left-5 lg:bottom-0 lg:left-0 p:3 lg:p-5">
               <UserIcon className="w-5 h-5 lg:w-10 lg:h-10 text-white" />
               <p className='ml-2.5 font-semibold text-white'>about me</p>
--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import { useInView } from 'react-intersection-observer';
 
 const AboutMe = () => {
@@ -33,10 +34,10 @@ const AboutMe = () => {
         <div className="about-me__left flex flex-col md:w-5/12">
           <h2 className={`${inView ? 'in-view-underline' : ''} text-sm md:text-2xl font-semibold relative`}>about me</h2>
           <p className="mt-2.5 relative about-me__para-1">
-            Hello! My name is Naveed Ahamed and I love building things for the internet. Throughout my journey of being a full stack developer, I've yearned to learn more things in the field! Along with my current knowledge, I'm eager to dip my fingers into something a lot more adventurous.
+            Hello! My name is Naveed Ahamed and I love building things for the internet. Throughout my journey of being a full stack developer, I&apos;ve yearned to learn more things in the field! Along with my current knowledge, I&apos;m eager to dip my fingers into something a lot more adventurous.
           </p>
           <p className="mt-2.5 md:mt-5 relative about-me__para-2">
-            Today however, I've worked for a maximum of 2 years and looking for adventure to further my knowledge in web development. I've worked for a <a href="/">small company</a> and a <a href="/">start up company</a> who's currently managing an ecommerce site that I was glad to be a part of!
+            Today however, I&apos;ve worked for a maximum of 2 years and looking for adventure to further my knowledge in web development. I&apos;ve worked for a <Link href="/">small company</Link> and a <Link href="/">start up company</Link> who&apos;s currently managing an ecommerce site that I was glad to be a part of!
           </p>
           <ul className="about-me__list relative mt-5">
             {languages.map((language, index) => (
@@ -49,7 +50,7 @@ const AboutMe = () => {
         </div>
         <div className="about-me__right flex flex-col w-full mt-12 md:mt-0 md:w-4/12 md:ml-auto md:mr-8">
           <div className="about-me__right-wrap block w-3/5 md:w-full relative">
-            <Image src="/images/aboutme.jpg" width={700} height={1000} layout='responsive' />
+            <Image src="/images/aboutme.jpg" width={700} height={1000} layout='responsive' alt="about-me-pic" />
           </div>
         </div>
       </div>
