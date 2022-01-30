a766fd56b718aad5a05e500d23f6ba363ad8dc10
AetheryNA <mrnaaw98@gmail.com>
2022-01-30 18:15:44 +0530

    About me linking

--- src/components/AboutCard.js
+++ src/components/AboutCard.js
@@ -1,11 +1,18 @@
 import {UserIcon} from '@heroicons/react/solid'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 const AboutCard = () => {
+  const { push } = useRouter()
+
+  const navigationToSection = () => {
+    push("#about-me")
+  }
+
   return (
-    <div className="about-card block relative rounded-lg max-w-max w-full cursor-pointer">
-      <Link href='/' >
+    <div className="about-card block relative rounded-lg max-w-max w-full cursor-pointer" onClick={navigationToSection}>
+      <Link href='#about-me'>
         <>
           <Image src="/images/aboutme.jpg" width={400} height={485} className="rounded-lg object-cover" />
             <div className="about-card__details flex flex-row items-center absolute bottom-5 left-5 lg:bottom-0 lg:left-0 p:3 lg:p-5">
