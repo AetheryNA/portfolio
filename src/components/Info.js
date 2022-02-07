cb2a1ed5bc00dfe5c455a17790930d374de4f82d
AetheryNA <mrnaaw98@gmail.com>
2022-02-07 23:46:00 +0530

    Info section component with animation :smile:

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,8 +1,14 @@
+import { useInView } from 'react-intersection-observer';
+
 const Info = () => {
+  const {ref, inView} = useInView({
+    threshold: .7,
+  })
+
   return (
-    <section className="info h-screen flex justify-center">
+    <section ref={ref} className="info h-screen flex justify-center items-center">
       <div className="container flex flex-col items-center">
-        <div className="info__wrap flex flex-col">
+        <div className={`info__wrap flex flex-col relative ${inView ? 'show-line' : 'hide-line'}`}>
           <h3 className="text-xl md:text-3xl">Like my profile?</h3>
           <h3 className="text-xl md:text-3xl">Let's work together!</h3>
         </div>
