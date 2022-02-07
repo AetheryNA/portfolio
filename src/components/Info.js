28cb30281887cb8663d2155823d04c245b8223d7
Naveed Ahamed <mrnaaw98@gmail.com>
2022-02-08 00:07:58 +0530

    [F012] Info Section (#18)
    * Information component
    
    * Info section component with animation :smile:
    
    * Fixed error
    
    * Bug fixes for warnings
    
    * Footer svg animations on hover

---
+++ src/components/Info.js
@@ -0,0 +1,20 @@
+import { useInView } from 'react-intersection-observer';
+
+const Info = () => {
+  const {ref, inView} = useInView({
+    threshold: .7,
+  })
+
+  return (
+    <section ref={ref} className="info h-screen flex justify-center items-center">
+      <div className="container flex flex-col items-center">
+        <div className={`info__wrap flex flex-col relative ${inView ? 'show-line' : 'hide-line'}`}>
+          <h3 className="text-xl md:text-3xl">Like my profile?</h3>
+          <h3 className="text-xl md:text-3xl">Let&apos;s work together!</h3>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Info;
