f5830f9418e249b2013297ff439f75373266cfb7
AetheryNA <mrnaaw98@gmail.com>
2022-02-16 13:02:40 +0530

    Added in view animations

--- src/components/PortfolioItem.js
+++ src/components/PortfolioItem.js
@@ -6,11 +6,12 @@ import { useInView } from 'react-intersection-observer'
 const PortfolioItem = ({ work_details }) => {
   const {ref, inView} = useInView({
     threshold: .5,
+    triggerOnce: true
   })
 
   return (
-    <article ref={ref} className={`work-infographic__card work-infographic__card--${work_details.position} flex flex-col md:flex-row items-center ${inView ? 'work-infographic__slide-in-' + work_details.position : ''}`}>
-      <div className="work-infographic__left md:w-2/6">
+    <article ref={ref} className={`portfolio-item__card portfolio-item__card--${work_details.position} opacity-0 flex flex-col md:flex-row items-center ${inView ? 'portfolio-item__slide-in-' + work_details.position : ''}`}>
+      <div className="portfolio-item__left md:w-2/6">
         <h2 className="flex flex-row items-center font-semibold uppercase mb-5 relative max-w-max">{work_details.title}<span className="font-normal text-xs text-grey">{work_details.date}</span></h2>
         <p>{work_details.desc}</p>
         <ul>
@@ -19,9 +20,9 @@ const PortfolioItem = ({ work_details }) => {
           ))}
         </ul>
       </div>
-      <div className="work-infographic__right w-full md:w-1/2 md:ml-auto">
+      <div className="portfolio-item__right w-full md:w-1/2 md:ml-auto">
         <Link href="https://google.com">
-          <div className="work-infographic__image relative">
+          <div className="portfolio-item__image relative">
             <Image src={work_details.image} width={580} height={350} layout='responsive' />
             <ExternalLinkIcon />
           </div>
