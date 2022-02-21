05adcc72ded51a164361b10a333239556d1be547
Naveed Ahamed <mrnaaw98@gmail.com>
2022-02-21 21:42:26 +0530

    Bug fix on card interaction (#21)

--- src/components/WorkInfographic.js
+++ src/components/WorkInfographic.js
@@ -46,7 +46,7 @@ const WorkInfographic = () => {
       date: '2020-2021',
       desc: 'An e-commerce website built on NextJS and ExpressJS. It was a locally funded site built for the Sri Lankan E-Commerce sector and was headed by MyPower.Lk',
       image: 'http://picsum.photos/2000',
-      link: 'http://google.com',
+      link: '/',
       achievements: [
         {
           item: 'Lead Front end development of the project'
@@ -106,7 +106,7 @@ const WorkInfographic = () => {
   return (
     <section className="work-infographic">
       <div className="container">
-        <h2 className="work-infographic__title font-semibold mb-16 md:mb-20 relative max-w-max">Stuff I&apos;ve Built</h2>
+        <h2 className="work-infographic__title font-semibold -mb-8 md:-mb-14 relative max-w-max">Stuff I&apos;ve Built</h2>
         {work_info.map((work_item, index) => (
           <PortfolioItem work_details={work_item} key={index} />
         ))}
