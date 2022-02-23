297e21b6b36b480177464632b05124cf4ba2da52
AetheryNA <mrnaaw98@gmail.com>
2022-02-23 20:56:51 +0530

    Changes to slideshow and styling on the timeline

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,8 +2,6 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { Pagination } from 'swiper'
-import NextIcon from '../../public/icons/nextjs.svg'
-import ReactIcon from '../../public/icons/react.svg'
 import { useRouter } from 'next/router'
 
 import 'swiper/css/pagination';
@@ -29,14 +27,10 @@ const Timeline = () => {
               <div className="timeline__slide-wrap timeline__slide-wrap--1 relative" onClick={() => {push('#care')}}>
                 <Image src={'/images/care.png'} alt="slide-1" width={735} height={485} className='rounded-xl object-cover' />
                 <div className="timeline__details absolute left-5 bottom-5">
-                  <h2> MyPower.LK</h2>
+                  <h2>CARE</h2>
                 </div>
                 <div className="timeline__details absolute right-5 bottom-5">
-                  <p className='text-white'>Ecommerce Website made for MyPowerLK</p>
-                </div>
-                <div className="timeline__details absolute flex flex-row right-5 top-5">
-                  <NextIcon />
-                  <ReactIcon />
+                  <p className='text-white'>Charity organisation website</p>
                 </div>
               </div>
             </>
@@ -48,6 +42,12 @@ const Timeline = () => {
             <>
               <div className="timeline__slide-wrap timeline__slide-wrap--2 relative" onClick={() => {push('#wbr')}}>
                 <Image src={'/images/wbr.png'} alt="slide-2" width={535} height={485} className='rounded-xl object-cover' />
+                <div className="timeline__details absolute left-5 bottom-5">
+                  <h2>World Bicycle Relief</h2>
+                </div>
+                <div className="timeline__details absolute right-5 bottom-5">
+                  <p className='text-white'>Charity organisation website</p>
+                </div>
               </div>
             </>
           </Link>
@@ -58,6 +58,12 @@ const Timeline = () => {
             <>
               <div className="timeline__slide-wrap timeline__slide-wrap--3 relative" onClick={() => {push('#mypower')}}>
                 <Image src={'/images/mypower.png'} alt="slide-3" width={800} height={485} className='rounded-xl object-cover' />
+                <div className="timeline__details absolute left-5 bottom-5">
+                  <h2>MYPOWER.LK</h2>
+                </div>
+                <div className="timeline__details absolute right-5 bottom-5">
+                  <p className='text-white'>Ecommerce website made for MyPower</p>
+                </div>
               </div>
             </>
           </Link>
@@ -68,8 +74,11 @@ const Timeline = () => {
             <>
               <div className="timeline__slide-wrap timeline__slide-wrap--4 relative" onClick={() => {push('#youlead')}}>
                 <Image src={'/images/youlead.png'} alt="slide-4" width={1085} height={485} className='rounded-xl object-cover' />
-                <div className="timeline__details absolute">
-                  <p className='text-white'> Hello world </p>
+                <div className="timeline__details absolute left-5 bottom-5">
+                  <h2>YOULEAD</h2>
+                </div>
+                <div className="timeline__details absolute right-5 bottom-5">
+                  <p className='text-white'>Training and educational services website</p>
                 </div>
               </div>
             </>
@@ -81,8 +90,11 @@ const Timeline = () => {
             <>
               <div className="timeline__slide-wrap timeline__slide-wrap--5 relative" onClick={() => {push('#dxdy')}}>
                 <Image src={'/images/dxdy.png'} alt="slide-4" width={1085} height={485} className='rounded-xl object-cover' />
-                <div className="timeline__details absolute">
-                  <p className='text-white'> Hello world </p>
+                <div className="timeline__details absolute left-5 bottom-5">
+                  <h2>DXDY</h2>
+                </div>
+                <div className="timeline__details absolute right-5 bottom-5">
+                  <p className='text-white'>Company showcase site made for DxDy</p>
                 </div>
               </div>
             </>
